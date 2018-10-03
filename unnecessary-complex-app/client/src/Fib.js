a5ac5e176bfbe0930e0701ed0ad077e899fc3668
anotherFeng <regnagleppod@gmail.com>
2018-10-03 04:26:19 -0400

    Fix worker issue where fib is not being calculated

--- unnecessary-complex-app/client/src/Fib.js
+++ unnecessary-complex-app/client/src/Fib.js
@@ -11,39 +11,44 @@ class Fib extends Component {
   componentDidMount() {
     this.fetchValues();
     this.fetchIndexes();
-  };
+  }
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
-  };
+  }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-    this.setState({ seenIndexes: seenIndexes.data });
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+
     await axios.post('/api/values', {
       index: this.state.index
     });
-    this.setState({index: ''});
-  }
+    this.setState({ index: '' });
+  };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({number}) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
     const entries = [];
+
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
-          For index {key} I calculated {this.state.values[key]}.
+          For index {key} I calculated {this.state.values[key]}
         </div>
       );
     }
+
     return entries;
   }
 
@@ -52,20 +57,21 @@ class Fib extends Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>Enter your index:</label>
-          <input value={this.state.index}
-            onChange={event => this.setState({index: event})}
-          >
-            
-          </input>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
           <button>Submit</button>
         </form>
+
         <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
+
         <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    )
+    );
   }
-};
+}
 
 export default Fib;
--- unnecessary-complex-app/worker/index.js
+++ unnecessary-complex-app/worker/index.js
@@ -1,4 +1,4 @@
-const keys = require('/keys.js');
+const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
@@ -8,10 +8,10 @@ const redisClient = redis.createClient({
 });
 const sub = redisClient.duplicate();
 
-const fib = (index) => {
-  if(index < 2) return 1;
+function fib(index) {
+  if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
-};
+}
 
 sub.on('message', (channel, message) => {
   redisClient.hset('values', message, fib(parseInt(message)));
